6dffc0f20d87227b999c262fd12be2b84dacc4c3
Arjun G. Menon <arjungmenon@gmail.com>
2019-06-28 23:59:32 +0400

    beats 76.01 % of python3 submissions > flipping-an-image

---
+++ leetcode-easy-832-flipping-an-image.py
@@ -0,0 +1,7 @@
+class Solution:
+    def flipAndInvertImage(self, A):
+        """
+        :type A: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        return [[int(not x) for x in reversed(row)] for row in A]
