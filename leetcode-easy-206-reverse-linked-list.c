ff68dd225271615aa7e079d8daf4f86bd690462e
Arjun G. Menon <arjungmenon@gmail.com>
2019-06-28 23:45:39 +0400

    beats 100.00 % of c submissions > reverse-linked-list

---
+++ leetcode-easy-206-reverse-linked-list.c
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* reverseList(struct ListNode* head) {
+    if (!head || !head->next)
+        return head;
+    
+    struct ListNode *prev = head, *node = head->next;
+    head->next = NULL;
+    
+    while(node) {
+        struct ListNode *next = node->next;
+        node->next = prev;
+        
+        prev = node;
+        node = next;
+    }
+    
+    return prev;
+}
+
+/*
+struct ListNode* reverseListRecursive(struct ListNode* head) {
+    if (!head || !head->next)
+        return head;
+
+    struct ListNode* reversedList = reverseListRecursive(head->next);
+
+    struct ListNode* end = reversedList;
+    while(end->next)
+        end = end->next;
+    end->next = head;
+    head->next = NULL;
+
+    return reversedList;
+}*/
+
